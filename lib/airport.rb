89e2d1ce0e4d341a0317e1b21298b9725f78cb19
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-09 12:07:41 +0000

    instructs planes to land except when full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,13 @@
 class Airport
+
+  def initialize(capacity)
+    @capacity = capacity
+    @planes_landed = 0
+  end
+
   def land(plane)
+    raise "Aiport is at capacity" if @planes_landed >= @capacity
+    @planes_landed +=1
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,9 +1,9 @@
 require 'airport'
 
 describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(10) }
   it 'so planes land at airport, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(10)
     plane = Plane.new
     airport.land(plane)
     expect { airport.land(plane) }.not_to raise_error
@@ -14,12 +14,28 @@ describe Airport do
   end
 
   it "planes can take off from airport, instruct a plane to take off" do
-    airport = Airport.new
+    airport = Airport.new(10)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
 
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'instructs a plane to land, except when airport is at capacity' do
+    airport = Airport.new(10)
+    plane = Plane.new
+    10.times do airport.land(plane)
+      end
+    expect { airport.land(plane) }.to raise_error "Aiport is at capacity"
+  end
+
+  it 'does not allow to land when airport full' do
+    plane = Plane.new
+    10.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error "Aiport is at capacity"
   end
 end
