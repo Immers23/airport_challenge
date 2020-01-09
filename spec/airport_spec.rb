e856aee7b08dd73734fb65ab836b63916361b586
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-09 00:02:36 +0000

    planes can take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,4 +12,14 @@ describe Airport do
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it "planes can take off from airport, instruct a plane to take off" do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
+  end
+
+  it 'instructs a plane to land' do
+    expect(airport).to respond_to(:take_off).with(1).argument
+  end
 end
